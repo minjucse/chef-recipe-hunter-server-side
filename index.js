9b79044c3b1aba15207b5136df21450022af17f3
minjucse <minjucse@gmail.com>
2023-05-04 17:39:34 +0600

    vercel json file added

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const cors = require('cors');
 
 app.use(cors());
 
+const allData = require('./data/recipeData.json');
+
+app.get('/', (req, res) =>{
+    res.send('Api is ruuning');
+});
+
+app.get('/data', (req, res) =>{
+    res.send(allData);
+});
+
+
+
 app.listen(port, () => {
     console.log(`API is running on port: ${port}`)
 })
